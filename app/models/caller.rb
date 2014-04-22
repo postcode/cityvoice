7b8253d05dd48b6dd177802fb44d672cdb84adcd
CJ Bryan <christianbryan@gmail.com>
2014-04-21 21:34:39 -0700

    Migrating

--- app/models/caller.rb
+++ app/models/caller.rb
@@ -2,10 +2,11 @@
 #
 # Table name: callers
 #
-#  id           :integer          not null, primary key
-#  phone_number :string(255)
-#  created_at   :datetime
-#  updated_at   :datetime
+#  id                    :integer          not null, primary key
+#  consented_to_callback :boolean
+#  phone_number          :string(255)
+#  created_at            :datetime
+#  updated_at            :datetime
 #
 
 class Caller < ActiveRecord::Base
