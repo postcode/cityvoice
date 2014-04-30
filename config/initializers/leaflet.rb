08247e369928e436d0de6838c827704e36fbd77c
CJ Bryan <christianbryan@gmail.com>
2014-04-29 17:01:30 -0700

    Simplify the frontend code for displaying the map.

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,3 +1,9 @@
-Leaflet.tile_layer = 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png'
-Leaflet.attribution = 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'
+if ENV['MAPBOX_MAP_ID'].present?
+  Leaflet.tile_layer = "http://api.tiles.mapbox.com/v3/#{ENV['MAPBOX_MAP_ID']}/{z}/{x}/{y}.png"
+  Leaflet.attribution = '<a href="https://www.mapbox.com/about/maps/" target="_blank">&copy;Mapbox &copy;OpenStreetMap</a> | <a class="mapbox-improve-map" href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a>'
+else
+  Leaflet.tile_layer = 'http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png'
+  Leaflet.attribution = 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>'
+end
+
 Leaflet.max_zoom = 12
