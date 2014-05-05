8d0b087db29c75ab3271dee98f4c93786b39a85f
CJ Bryan <christianbryan@gmail.com>
2014-05-05 11:54:36 -0700

    Show all of the questions on the activity page and fix the vote result bar scaling issues.

--- app/controllers/numerical_answers_controller.rb
+++ app/controllers/numerical_answers_controller.rb
@@ -1,11 +1,14 @@
 class NumericalAnswersController < ApplicationController
   def index
-    total_counts = Answer.total_calls
-    repair_counts = Answer.total_responses(1)
-    remove_counts = Answer.total_responses(2)
-    counts = AnswerCounts.new(total_counts, repair_counts, remove_counts)
-    @counts_hash = counts.to_hash
-    @sorted_array = counts.to_array
+    @locations = Location.all
+
+    # total_counts = Answer.total_calls
+    # repair_counts = Answer.total_responses(1)
+    # remove_counts = Answer.total_responses(2)
+    # counts = AnswerCounts.new(total_counts, repair_counts, remove_counts)
+    # @counts_hash = counts.to_hash
+    # @sorted_array = counts.to_array
+
     respond_to do |format|
       format.html
       format.csv { send_data build_csv }
